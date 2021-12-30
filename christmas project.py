3c9c762ff8c259b7b8106b68ebe7406ec4c09240
Aysha fidha <92437867+Neilopher@users.noreply.github.com>
2021-12-30 14:50:12 +0530

    Add files via upload

---
+++ christmas project.py
@@ -0,0 +1,13 @@
+num = int(input("enter the number of rows: ")) #7
+
+for i in range(1,num+1):
+    print(i)
+    print(" "*(num-i)+"* "*i)
+
+for j in range(num):
+    print(" "*(num-1)+"*")
+
+
+
+
+
